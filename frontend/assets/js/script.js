454a7b88d6b122d700100c67c56156efe99577cc
GabrielFer555 <kaiberimanos3@gmail.com>
2023-10-25 21:13:08 -0300

    Construindo Frontend

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,1 +1,8 @@
+const fornCadastradas = document.getElementsByClassName("span-num")[0];
+const colunas = document.getElementsByClassName("columns");
+fornCadastradas.textContent = colunas.length.toString();
+const textNumb =  document.getElementsByClassName("nullTable")[0];
+if(colunas.length > 0){
+    textNumb.setAttribute("class","d-none");
+}
 
