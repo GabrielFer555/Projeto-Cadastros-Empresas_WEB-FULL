76b12118c74b1a915a6631b13f8662fb8ec2ffae
GabrielFer555 <kaiberimanos3@gmail.com>
2023-10-27 13:02:18 -0300

    Dado continuação no front

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,6 +1,3 @@
-const notyf = new Notyf();
-
-
 const fornCadastradas = document.getElementsByClassName("span-num")[0];
 const colunas = document.getElementsByClassName("columns");
 fornCadastradas.textContent = colunas.length.toString();
@@ -9,19 +6,3 @@ if(colunas.length > 0){
     textNumb.setAttribute("class","d-none");
 }
 
-function mensagem(msg,type=1, duration = 1000, x ="left", y ="bottom"){
-    const i = new Notyf({
-        duration:duration,
-        position:{
-            x:x,
-            y:y
-        },
-    });
-switch(type){
-    case 1:
-        i.success(msg);
-        break;
-    case 2:
-        i.error(msg);
-        break;
-}}
