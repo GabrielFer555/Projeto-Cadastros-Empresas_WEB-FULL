7dd96b03e93478cba8dfc1451eaa68e1de2512e2
GabrielFer555 <kaiberimanos3@gmail.com>
2023-10-26 22:29:23 -0300

    Continuação do desenvolvimento do Frontend

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,3 +1,6 @@
+const notyf = new Notyf();
+
+
 const fornCadastradas = document.getElementsByClassName("span-num")[0];
 const colunas = document.getElementsByClassName("columns");
 fornCadastradas.textContent = colunas.length.toString();
@@ -6,3 +9,19 @@ if(colunas.length > 0){
     textNumb.setAttribute("class","d-none");
 }
 
+function mensagem(msg,type=1, duration = 1000, x ="left", y ="bottom"){
+    const i = new Notyf({
+        duration:duration,
+        position:{
+            x:x,
+            y:y
+        },
+    });
+switch(type){
+    case 1:
+        i.success(msg);
+        break;
+    case 2:
+        i.error(msg);
+        break;
+}}
