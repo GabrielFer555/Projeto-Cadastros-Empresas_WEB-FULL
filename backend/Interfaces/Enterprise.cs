3a757808e13102fc68a56c06ebbbb38296954aa5
GabrielFer555 <kaiberimanos3@gmail.com>
2023-11-02 09:33:13 -0300

    Update Enterprise.cs

--- backend/Interfaces/Enterprise.cs
+++ backend/Interfaces/Enterprise.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace backend.Interfaces
 {
-    public interface Enterprise
+    public interface IEnterprise
     {
         String name { get; set;}
         String document{ get; set; }
