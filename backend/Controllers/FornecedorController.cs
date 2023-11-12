5554c2190a46d484c4eb9c9b9126ba05c139b7ec
GabrielFer555 <kaiberimanos3@gmail.com>
2023-11-12 19:09:07 -0300

    Update FornecedorController.cs

--- backend/Controllers/FornecedorController.cs
+++ backend/Controllers/FornecedorController.cs
@@ -18,10 +18,26 @@ namespace backend.Controllers
        [HttpGet]
        [Route("Producer")]
        public async Task<IActionResult> GetOverall([FromServices] AppDbContext dbContext){
+        try{
             var producers =  await dbContext.fornecedors_cad.AsNoTracking().ToListAsync();
             return Ok(producers);
+        }catch(Exception e){
+            return BadRequest(e);
+        }
+            
        }
 
-       
+       [HttpGet]
+       [Route("Producer/{id}")]
+       public async Task<IActionResult> GetbyId([FromServices] AppDbContext dbContext, [FromRoute] int id){
+            var Fornecedor = dbContext.fornecedors_cad.FirstOrDefaultAsync(x => x.Id == id);
+            if(Fornecedor == null){
+                return NotFound();
+            }else{
+                return Ok(Fornecedor);
+            }
+       }
+
+
     }
 }
