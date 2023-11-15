2c3bd013e0ceccd121c4845143e2fd495e33196e
GabrielFer555 <kaiberimanos3@gmail.com>
2023-11-15 16:29:12 -0300

    Adicionado Front end e quase finalizado API

--- backend/Controllers/FornecedorController.cs
+++ backend/Controllers/FornecedorController.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using backend.Classes;
 using backend.Data;
+using backend.ModelViews.Classes;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Extensions.Logging;
 
 namespace backend.Controllers
@@ -14,29 +17,106 @@ namespace backend.Controllers
     [Route("v1")]
     public class FornecedorController : Controller
     {
-       
-       [HttpGet]
-       [Route("Producer")]
-       public async Task<IActionResult> GetOverall([FromServices] AppDbContext dbContext){
-        try{
-            var producers =  await dbContext.fornecedors_cad.AsNoTracking().ToListAsync();
-            return Ok(producers);
-        }catch(Exception e){
-            return BadRequest(e);
-        }
-            
-       }
 
-       [HttpGet]
-       [Route("Producer/{id}")]
-       public async Task<IActionResult> GetbyId([FromServices] AppDbContext dbContext, [FromRoute] int id){
-            var Fornecedor = dbContext.fornecedors_cad.FirstOrDefaultAsync(x => x.Id == id);
-            if(Fornecedor == null){
-                return NotFound();
-            }else{
-                return Ok(Fornecedor);
+        [HttpGet]
+        [Route("Producer/FindAll")]
+        public async Task<IActionResult> GetOverall([FromServices] AppDbContext dbContext)
+        {
+            try
+            {
+                var producers = await dbContext.fornecedors_cad.AsNoTracking().ToListAsync();
+                return Ok(producers);
             }
-       }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+
+        }
+
+        [HttpGet]
+        [Route("Producer/Find/{id}")]
+        public async Task<IActionResult> GetbyId([FromServices] AppDbContext dbContext, [FromRoute] int id)
+        {
+            var fornecedor = await dbContext.fornecedors_cad.FirstOrDefaultAsync(x => x.Id == id);
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(fornecedor);
+            }
+        }
+        [HttpPost]  //!Under Development
+        [Route("Producer/Create")]
+        public async Task<IActionResult> Post([FromServices] AppDbContext dbContext, [FromBody] Fornecedor fornecedor)
+        {
+            if(fornecedor.EmpresaVinculada == 0){
+                return BadRequest("campo 'EmpresaVinculada' é invalida");
+            }
+            try{
+            var Emp = await dbContext.empresa_cad.FirstOrDefaultAsync(x => x.EmpresaId == fornecedor.EmpresaVinculada);
+            if (Emp == null)
+            {
+                return BadRequest("Empresa não existe");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            if (fornecedor.TipoFornecedor == "f")
+            {
+                String Uf = Emp.Uf;
+                DateOnly dataNasc = (DateOnly)fornecedor.DataNasc;
+                if (Uf == "PR" && calcularIdade(dataNasc) < 18)
+                {
+                    return BadRequest("Pessoa fisica menor de idade não pode ser Vinculado a empresa do Paraná");
+                }
+                else
+                {
+                    try{
+                        await dbContext.AddAsync(fornecedor);
+                        await dbContext.SaveChangesAsync();
+                        return Created($"v1/Producer/Find/{fornecedor.Id}",fornecedor);
+                    }catch(Exception e){
+                        return BadRequest("Erro Interno!");
+                    }
+                }
+            }
+            else
+            {
+                if(fornecedor.Document.Length < 19){
+                    return BadRequest("CNPJ invalido");
+                }
+                else{
+                    try{
+                        await dbContext.AddAsync(fornecedor);
+                        await dbContext.SaveChangesAsync();
+                        return Created($"v1/Producer/Find/{fornecedor.Id}",fornecedor);
+                    }   catch(Exception e){
+                        return BadRequest(e.GetBaseException());
+                    }
+                }
+            }
+            }catch(Exception e){
+                return BadRequest("Erro Interno!");
+            }
+            
+        }
+        [NonAction]
+        public int calcularIdade(DateOnly data)
+        {
+            DateOnly dataAtual = DateOnly.FromDateTime(DateTime.Now);
+            int diff = dataAtual.Year - data.Year;
+            if (data.DayOfYear < dataAtual.DayOfYear)
+            {
+                diff--;
+            }
+
+            return diff;
+        }
+
 
 
     }
--- backend/Data/AppDbContext.cs
+++ backend/Data/AppDbContext.cs
@@ -13,7 +13,7 @@ namespace backend.Data
         public DbSet<Empresa> empresa_cad { get; set; }/*Representa a tabela de empresa*/
         public DbSet<Fornecedor> fornecedors_cad { get; set;}/*Representa a tabela de fornecedores*/
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite("DataSource =app.db; Cache=Shared");
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseNpgsql(@"Server=dpg-cl9v105o7jlc73fjttp0-a.oregon-postgres.render.com;Port=5432;Database=mydb_hqtj;User Id=gabriel;Password=7bPmAKYo9BQguU6TPlGn3etazLlW6WCj;");
         
     }
 }
