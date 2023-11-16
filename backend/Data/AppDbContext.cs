61a7160420be30b3c6debefccae3c53c9b8a14bb
GabrielFer555 <kaiberimanos3@gmail.com>
2023-11-16 14:55:47 -0300

    Ajustando configuração de banco de dados

--- backend/Data/AppDbContext.cs
+++ backend/Data/AppDbContext.cs
@@ -10,10 +10,20 @@ namespace backend.Data
 {
     public class AppDbContext: DbContext /*Faz o De-para das classses para o Banco*/
     {
+        private readonly IConfiguration _configuration;
+        
+        public AppDbContext(IConfiguration configuration){
+            _configuration = configuration;
+        }
         public DbSet<Empresa> empresa_cad { get; set; }/*Representa a tabela de empresa*/
         public DbSet<Fornecedor> fornecedors_cad { get; set;}/*Representa a tabela de fornecedores*/
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseNpgsql(@"Server=dpg-cl9v105o7jlc73fjttp0-a.oregon-postgres.render.com;Port=5432;Database=mydb_hqtj;User Id=gabriel;Password=7bPmAKYo9BQguU6TPlGn3etazLlW6WCj;");
-        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            if(!optionsBuilder.IsConfigured){
+                String connectionString = _configuration.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseNpgsql(@"Server=dpg-cl9v105o7jlc73fjttp0-a.oregon-postgres.render.com;Port=5432;Database=mydb_hqtj;User Id=gabriel;Password=7bPmAKYo9BQguU6TPlGn3etazLlW6WCj;");
+            }
+            
+        }
     }
 }
